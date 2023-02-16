713adae0287f1c6f1b823ce744d70de748ff2647
Jedrzej Kowalski <rbxxx2k@gmail.com>
2023-02-16 17:57:00 +0100

    removed sprite

--- clay.py
+++ clay.py
@@ -14,17 +14,17 @@ class Clay(pygame.sprite.Sprite):
         self.radius = radius
         self.offset = offset
         self.color_manager = color.Color()
-        self.reference_sprite = pygame.image.load("clay.png").convert_alpha()
-        self.sprite = copy.copy(self.reference_sprite)
+        # self.reference_sprite = pygame.image.load("clay.png").convert_alpha()
+        # self.sprite = copy.copy(self.reference_sprite)
         self.set_position(pygame.math.Vector2(0, 0))
-        self.update()
+        # self.update()
 
     def update(self):
         self.sprite = pygame.transform.scale(self.reference_sprite, pygame.math.Vector2(2*self.radius, 2*self.radius))
 
     def set_position(self, new_position):
         self.position = new_position + self.offset
-        self.update()
+        # self.update()
 
     def set_offset(self, new_offset):
         self.offset.update(new_offset)
